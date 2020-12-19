a0ea2e2b78d22916bff358eb959072524561ca55
Ming Wu <2133861w@student.gla.ac.uk>
2020-12-19 15:43:37 +0000

    add example react codes to GitLab

---
+++ Documentations/Example Codes for Reference - React/1. react-example-code-essential-training-19Dec20/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// function AppTwo() {
+//   return <h1>This is the Second App</h1>;
+// }
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const [,, light] = ["boots", "tent", "headlamp"];
+// console.log(light +" list deconstruction");
+
+// ReactDOM.render(
+//   <>
+//     <App authorized={false}/>,
+//     <AppTwo />
+//   </>,
+//   document.getElementById("root")
+// );
