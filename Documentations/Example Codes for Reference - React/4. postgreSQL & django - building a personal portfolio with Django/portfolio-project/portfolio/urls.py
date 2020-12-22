c77a7c74e0388b542f3dc245d719a76118a75924
Ming Wu <2133861w@student.gla.ac.uk>
2020-12-22 20:48:16 +0000

    react-django-postgressql-interaction

--- Documentations/Example Codes for Reference - React/4. postgreSQL & django - building a personal portfolio with Django/portfolio-project/portfolio/urls.py
+++ Documentations/Example Codes for Reference - React/4. postgreSQL & django - building a personal portfolio with Django/portfolio-project/portfolio/urls.py
@@ -8,7 +8,7 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatt erns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
@@ -19,5 +19,5 @@ import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', jobs.views.home, name='home'),
+    path('', jobs.views.home, name='home'),
 ]
