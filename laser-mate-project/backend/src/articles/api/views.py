0205e9972681f5c6f2cf216b4a0a06399f12852a
Ming Wu <2133861w@student.gla.ac.uk>
2020-12-30 21:59:03 +0000

    finish real estate template project

--- laser-mate-project/backend/src/articles/api/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from articles.models import Article
-from .serializers import ArticleSerializer
-
-# handle all views (list, detail, create, delete, update)
-
-class ArticleViewSet(viewsets.ModelViewSet):
-    serializer_class = ArticleSerializer
-    queryset = Article.objects.all()
