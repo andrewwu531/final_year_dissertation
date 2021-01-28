69dd131954eb3140cad236daaa1ba71be813b631
Ming Ho Wu <2133861w@student.gla.ac.uk>
2021-01-28 18:35:22 +0000

    dissertation work

--- laser-mate-project/frontend/laser_mate/src/App.js
+++ laser-mate-project/frontend/laser_mate/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import FirstPage from './containers/mobile_web/1FirstPage';
+import FirstPagev1 from './containers/mobile_web/FirstPagev1';
 import Order_Summary from './containers/mobile_web/3Order_Summary';
 import Payment_Portal from './containers/mobile_web/4Payment_Portal';
 import Not_Found from './containers/mobile_web/5NotFound';
@@ -12,6 +13,7 @@ import './sass/main.scss';
 const App = () => (
     <Router>
         <Switch> 
+            <Route exact path='/:restaurant_id/second' component={FirstPagev1} />
             <Route exact path='/:restaurant_id/:table_number' component={FirstPage} />
             <Route exact path='/' component={Home} />
             <Route component={Not_Found} />
